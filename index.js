73bfeddb53fc6056df1b3841a65310f247156d2d
radu21-jd <120336732+radu21-jd@users.noreply.github.com>
2022-12-11 23:14:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_wxil9ag";
+  const templateID = "template_gkypghn";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Confirmarea platii a fost trimisa pe e-mailul dvs!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
+
